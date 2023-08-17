48a641ebb6df234831cd4093384e7a0d86c69814
Hariz <syedhariz1995@gmail.com>
2023-08-17 17:12:07 +0800

    looped till max of 5 scores achieved and exit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let playerScore = 0
+let computerScore = 0 
+
+
+
 function getComputerChoice(){
     let choice = Math.random()
      if(choice <= 0.33){
@@ -9,31 +14,50 @@ function getComputerChoice(){
      }
 }
 
-
-
-
 function playRound(player, computer){
     console.log(`Computer plays ${computer}`)
     console.log(`Player plays ${player}`)
-    if(player === "rock" && computer === "scissors"){
-        return `Player Win! Rock beats Scissors`
-    } else if (player === "scissors" && computer === "paper"){
-        return `Player Win! Scissors beats paper`
-    }else if (player === "paper" && computer === "rock"){
-        return`Player win ! Paper beats rock `
-    }else if(player === "scissors" && computer === "rock"){
-        return(`Computer Wins! Rock beats scissor`)
-    } else if( player === "paper" && computer === "scissors"){
-        return (`Computer wins! Scissors beat paper`)
-    }else if(player === "rock" && computer === "paper"){
-        return(`Computer wins! Paper beats rock`)
-    } else if (player === computer){
-        return `It's a draw!!`
-    }else{
-        return `Invalid input, try again`
+    for(i = 0; i <= 5; i++){
+        if(player === "rock" && computer === "scissors"){
+            playerScore++
+            return `Player Win! Rock beats Scissors`
+        } else if (player === "scissors" && computer === "paper"){
+            playerScore++
+            return `Player Win! Scissors beats paper`
+        }else if (player === "paper" && computer === "rock"){
+            playerScore++
+            return`Player win ! Paper beats rock `
+        }else if(player === "scissors" && computer === "rock"){
+            computerScore++
+            return(`Computer Wins! Rock beats scissor`)
+        } else if( player === "paper" && computer === "scissors"){
+            computerScore++
+            return (`Computer wins! Scissors beat paper`)
+        }else if(player === "rock" && computer === "paper"){
+            computerScore++
+            return(`Computer wins! Paper beats rock`)
+        } else if (player === computer){
+            return `It's a draw!!`
+        }else{
+            return `Invalid input, try again`
+        }
+    }
+    
+}
+
+function playGame(){
+    while (playerScore < 5 && computerScore < 5){
+        const player = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
+        const computer = getComputerChoice();
+        console.log(playRound(player,computer))
+        console.log(`Player's Score : ${playerScore} - Computer's Score : ${computerScore} \n\n\n`)
+    }
+
+    if(playerScore > computerScore){
+        console.log("Player wins!!!!!")
+    } else if (computerScore > playerScore){
+        console.log("Computer wins!!!!!")
     }
 }
 
-const player = (prompt("Choose Rock, Paper, or Scissors")).toLowerCase()
-const computer = getComputerChoice()
-console.log(playRound(player, computer))
+playGame()
